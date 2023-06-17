69bf058132582153e434f212960e6a6160e0b79f
Musojon <musojontursunov925@gmail.com>
2023-06-17 17:51:54 +0500

    completed copy function

--- src/codeflask.js
+++ src/codeflask.js
@@ -92,7 +92,7 @@
      this._preserveBlank = config.preserveBlank !== undefined ? config.preserveBlank : false;
 
      this._element; // used to hold the wrapper div, as a point of reference
-
+     this._copySvg='<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>'
 
 
      // let x = (x === undefined) ? your_default_value : x;
@@ -138,12 +138,25 @@
     editorElem.classList.add('editorjs-codeFlask_Editor')
     let langdisplay = document.createElement('div');
     langdisplay.classList.add('editorjs-codeFlask_LangDisplay')
+    let coppyButton = document.createElement('button')
+    coppyButton.classList.add("editorjs-codeFlask_CopyButton");
+    coppyButton.innerHTML = this._copySvg;
 
     langdisplay.innerHTML = this.data.language
-
+    coppyButton.addEventListener("click",async () => {
+      coppyButton.innerHTML =
+        '<svg fill="#000000" height="1em" width="1em" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512.42 512.42" xml:space="preserve"><g><g><path d="M509.313,67.45c-4.16-4.16-10.88-4.267-15.04-0.107L134.06,422.33l-115.2-121.6c-3.733-4.48-10.453-5.12-15.04-1.387 c-4.48,3.733-5.12,10.453-1.387,15.04c0.32,0.32,0.533,0.64,0.853,0.96l122.667,129.493c1.92,2.133,4.693,3.307,7.573,3.307h0.213 c2.773,0,5.44-1.067,7.467-3.093l368-362.667C513.473,78.33,513.473,71.61,509.313,67.45z"/></g></g></svg>';
+      try {
+       await navigator.clipboard.writeText(this.data.editorInstance.code);
+      } finally {
+        setTimeout(()=>{
+           coppyButton.innerHTML = this._copySvg;
+        },1000)
+      }
+    });
     this._element.appendChild(editorElem)
     this._element.appendChild(langdisplay)
-
+    this._element.appendChild(coppyButton);
     this.data.editorInstance = new CodeFlask(editorElem, {
       language: this.data.language,
       lineNumbers : this.data.showlinenumbers,
